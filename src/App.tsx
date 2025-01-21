fe0eee8bf32f88ed6008bd14c6462dc836f0596c
Mutairu-Lawal <thebillionnairadream@gmail.com>
2025-01-21 17:00:27 +0000

    `Update App component to render HomePage on root route`

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+// relative paths
 import ForgotPassword from './pages/(auth)/ForgotPassword';
 import LoginPage from './pages/(auth)/LoginPage';
 import SignUpPage from './pages/(auth)/SignUpPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<SignUpPage />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
