4bc59e8386f7963b55bf88f13496a165e7019265
Mutairu-Lawal <thebillionnairadream@gmail.com>
2025-01-21 16:29:59 +0000

    `Added React Router for client-side routing`

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import ForgotPassword from './pages/ForgotPassword';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
@@ -5,9 +7,15 @@ import SignUpPage from './pages/SignUpPage';
 function App() {
   return (
     <>
-      {/* <LoginPage /> */}
-      <SignUpPage />
-      {/* <ForgotPassword /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<SignUpPage />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
