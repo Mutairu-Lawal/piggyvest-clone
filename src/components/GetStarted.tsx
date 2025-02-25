7a54855e7bc27712237e75e50af0464a7eae0037
Mutairu-Lawal <thebillionnairadream@gmail.com>
2025-02-25 12:23:58 +0000

    adjust the border-radius

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -14,11 +14,11 @@ export const GetStarted = ({
   return (
     <div className="space-y-2 my-3">
       <p className="text-sm uppercase font-normal text-gray-700">{title}</p>
-      <div className="rounded-lg overflow-hidden">
+      <div className="rounded-xl overflow-hidden">
         <div
           className={`border ${
             isHome
-              ? 'bg-primary text-white flex items-center justify-between py-3 px-7'
+              ? 'bg-primary text-white flex items-center justify-between py-3 px-7 rounded-xl'
               : 'h-[120px] bg-blue-800'
           } `}
         >
