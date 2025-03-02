8d5364309487ce615a3546c047a46398e7da7655
Mutairu-Lawal <thebillionnairadream@gmail.com>
2025-03-02 12:32:47 +0000

    `feat: added react-toastify for error notifications in login page`

--- src/pages/(auth)/LoginPage.tsx
+++ src/pages/(auth)/LoginPage.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { Bounce, ToastContainer, toast } from 'react-toastify';
 
 import { users } from '../../data/users';
 import AuthDashboard from '../../components/AuthDashboard';
@@ -49,74 +50,87 @@ export default function LoginPage() {
     if (userData[0].password === password) {
       navigate('/');
     } else {
-      alert('incorrect password or username');
+      toast.error('Incorrect email or password', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'light',
+        transition: Bounce,
+      });
     }
   };
 
   return (
-    <AuthDashboard>
-      <Box
-        title="Login to your account"
-        subtitle="Securely login to your PiggyVest"
-      >
-        <form
-          onSubmit={handleSubmit(handleOnSubmit)}
-          className="pb-3 px-1 mt-5 text-left space-y-7"
+    <>
+      <AuthDashboard>
+        <Box
+          title="Login to your account"
+          subtitle="Securely login to your PiggyVest"
         >
-          <div className="form-group">
-            <label>Email or Phone Number</label>
-            <input
-              {...register('emailOrPhoneNumber', {
-                required: { value: true, message: 'field is required' },
-              })}
-              type="text"
-              id="emailOrPhoneNumber"
-              name="emailOrPhoneNumber"
-            />
-            {errors.emailOrPhoneNumber && (
-              <p className="text-red-500">
-                {errors.emailOrPhoneNumber.message}
-              </p>
-            )}
-          </div>
-
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              {...register('password', {
-                required: {
-                  value: true,
-                  message: 'password required',
-                },
-              })}
-              type="password"
-              id="password"
-              name="password"
-              autoComplete=""
-            />
-            {errors.password && (
-              <p className="text-red-500">{errors.password.message}</p>
-            )}
-          </div>
-          <button
-            type="submit"
-            className="uppercase bg-[#0d60d8] hover:bg-[#0d4dd8] text-white px-5 py-3 rounded-lg w-full font-extrabold rounded-bl-none"
+          <form
+            onSubmit={handleSubmit(handleOnSubmit)}
+            className="pb-3 px-1 mt-5 text-left space-y-7"
           >
-            log in
-          </button>
-        </form>
-      </Box>
+            <div className="form-group">
+              <label>Email or Phone Number</label>
+              <input
+                {...register('emailOrPhoneNumber', {
+                  required: { value: true, message: 'field is required' },
+                })}
+                type="text"
+                id="emailOrPhoneNumber"
+                name="emailOrPhoneNumber"
+              />
+              {errors.emailOrPhoneNumber && (
+                <p className="text-red-500">
+                  {errors.emailOrPhoneNumber.message}
+                </p>
+              )}
+            </div>
 
-      {footerLinks.map(({ label, href, id, linkName }) => (
-        <Link
-          key={id}
-          to={href}
-          className="hover:text-gray-300 md:text-sm text-xs font-medium"
-          aria-label={`click to vist ${linkName} page`}
-        >
-          <h6 className="mt-7">{label}</h6>
-        </Link>
-      ))}
-    </AuthDashboard>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                {...register('password', {
+                  required: {
+                    value: true,
+                    message: 'password required',
+                  },
+                })}
+                type="password"
+                id="password"
+                name="password"
+                autoComplete=""
+              />
+              {errors.password && (
+                <p className="text-red-500">{errors.password.message}</p>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="uppercase bg-[#0d60d8] hover:bg-[#0d4dd8] text-white px-5 py-3 rounded-lg w-full font-extrabold rounded-bl-none"
+            >
+              log in
+            </button>
+          </form>
+        </Box>
+
+        {footerLinks.map(({ label, href, id, linkName }) => (
+          <Link
+            key={id}
+            to={href}
+            className="hover:text-gray-300 md:text-sm text-xs font-medium"
+            aria-label={`click to vist ${linkName} page`}
+          >
+            <h6 className="mt-7">{label}</h6>
+          </Link>
+        ))}
+      </AuthDashboard>
+      <ToastContainer />
+    </>
   );
 }
