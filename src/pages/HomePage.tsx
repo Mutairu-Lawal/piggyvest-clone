9a060d30dce2c316410c44a46981203ff1f94f55
Mutairu-Lawal <thebillionnairadream@gmail.com>
2025-03-17 23:44:11 +0100

    fix the homepage to load from the client state

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import Header from '../components/Header';
 import GetStarted from '../components/GetStarted';
@@ -11,49 +11,33 @@ import HomeShortcut from '../components/homepage/HomeShortcut';
 import AccountBalance from '../components/AccountBalance';
 import QuickSave from '../components/homepage/QuickSave';
 
-import { UserProps } from '../data/users';
 import { getTheDayTime, getTotalBalance } from '../utils/fun';
 import { useAppSelector } from '../app/hooks';
 import ShowModal from '../components/homepage/ShowModal';
 
 export default function HomePage() {
-  const [user, setUser] = useState<UserProps | null>(null);
   const [showQuickSave, setShowQuickSave] = useState(false);
   const [toastModal, setToastModal] = useState(false);
   const [serverResponse, setServerResponse] = useState<string | null>(null);
   const currentUser = useAppSelector((state) => state.currentUserData.user);
-
-  useEffect(() => {
-    const userId = currentUser.id;
-    const fectchUser = async () => {
-      try {
-        const res = await fetch(`/api/users/${userId}`);
-        const data = await res.json();
-        setUser(data);
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          console.log(error.message);
-        }
-      }
-    };
-
-    fectchUser();
-  });
 
   return (
     <>
       {!showQuickSave && (
         <main>
           <Header
-            title={`${user?.fullName.slice(0, user.fullName.indexOf(' '))},`}
+            title={`${currentUser.fullName.slice(
+              0,
+              currentUser.fullName.indexOf(' ')
+            )},`}
             subtitle={getTheDayTime()}
           />
           <HomeShortcut setStateAction={setShowQuickSave} />
-          {user && (
+          {currentUser && (
             <AccountBalance
               title="Total Savings"
               isHome
-              balance={getTotalBalance(user)}
+              balance={getTotalBalance(currentUser)}
             />
           )}
 
@@ -74,11 +58,11 @@ export default function HomePage() {
           <Transactions />
         </main>
       )}
-      {showQuickSave && user && (
+      {showQuickSave && currentUser && (
         <QuickSave
           setShowQuickSave={setShowQuickSave}
           setToastModal={setToastModal}
-          user={user}
+          user={currentUser}
           setServerResponse={setServerResponse}
         />
       )}
